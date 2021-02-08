f100a7d7713ab70b3bde8cbc40cba518bf773dc1
Mitchell Rodríguez <mitrodle@gmail.com>
2021-02-07 20:49:18 -0500

    Add: addTodo + uuid

---
+++ src/components/TodoForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import {v4 as uuidv4} from "uuid";
+
+function TodoForm({ addTodo }) {
+    const [todo, setTodo] = useState({
+        id: "",
+        task: "",
+        completed: false
+    });
+
+    function handleTaskInputChange(e) {
+        setTodo({ ...todo, task: e.target.value });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if(todo.task.trim()) {
+            addTodo({ ...todo, id: uuidv4() });
+        }
+
+        setTodo({ ...todo, task: "" }); // Reset input
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input name="task" type="text" value={todo.task} onChange={handleTaskInputChange}/>
+            <button type="submit">Save</button>
+        </form>
+    );
+}
+
+export default TodoForm;
