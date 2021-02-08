1e2e7c70c0768745423e41174532a79b46875bb5
Mitchell Rodríguez <mitrodle@gmail.com>
2021-02-07 23:18:17 -0500

    Add: Delete Todo + Check Todo [Complete]

--- src/App.js
+++ src/App.js
@@ -3,12 +3,44 @@ import './App.css';
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
+const LOCAL_STORAGE_KEY = "react-todo-list-todos";
+
 function App() {
 
   const [todos, setTodos] = useState([]);
 
+  useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(storageTodos) {
+      setTodos(storageTodos);
+    }
+  }, []);
+
+  // Local Storage
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
   function addTodo(todo) {
     setTodos([todo, ...todos]);
+  }
+
+  function toggleComplete(id) {
+    setTodos(
+      todos.map(todo => {
+        if(todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id));
   }
 
   return (
@@ -16,7 +48,7 @@ function App() {
       <header className="App-header">
         <p>Todo List with React</p>
         <TodoForm addTodo={addTodo}/>
-        <TodoList todos={todos}/>
+        <TodoList todos={todos} toggleComplete={toggleComplete} removeTodo={removeTodo}/>
       </header>
     </div>
   );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import Todo from './Todo';
+import React from "react";
+import Todo from "./Todo";
 
-function TodoList({ todos }){
-    return (
-        <ul>
-            {todos.map(todo => (
-                <Todo key={todo.id} todo={todo} />
-            ))}
-        </ul>
-    );
+function TodoList({ todos, toggleComplete, removeTodo }) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <Todo key={todo.id} 
+        todo={todo} 
+        toggleComplete={toggleComplete}
+        removeTodo={removeTodo}
+        />
+      ))}
+    </ul>
+  );
 }
 
 export default TodoList;
